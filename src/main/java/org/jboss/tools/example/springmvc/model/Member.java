3b1e4ea2da9f6e1cf57d70b694835ba97089ac85
alex <alex@alex-PC>
2017-06-07 18:48:21 -0300

    dsdsd

--- src/main/java/org/jboss/tools/example/springmvc/model/Member.java
+++ src/main/java/org/jboss/tools/example/springmvc/model/Member.java
@@ -1,19 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jboss.tools.example.springmvc.model;
 
 import java.io.Serializable;
@@ -24,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -36,60 +19,84 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 @Table(name = "MemberBasic", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
-    /**
-     * Default value included to remove warning. Remove or modify at will. *
-     */
-    private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	private static final long serialVersionUID = 1L;
 
-    @NotNull
-    @Size(min = 1, max = 25)
-    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
-    private String name;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    @NotNull
-    @NotEmpty(message = "Email address cannot be empty")
-    @Email(message = "Invalid email address, e.g. valid email address: example@gmail.com")
-    private String email;
+	@NotNull
+	@Size(min = 1, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	private String name;
 
-    @NotNull
-    @Size(min = 10, max = 12)
-    @Digits(fraction = 0, integer = 12, message = "Incorrect Format, valid e.g. 121212121212")
-    @Column(name = "phone_number")
-    private String phoneNumber;
+	@NotNull
+	@NotEmpty(message = "Email address cannot be empty")
+	@Email(message = "Invalid email address, e.g. valid email address: example@gmail.com")
+	private String email;
 
-    public Long getId() {
-        return id;
-    }
+	@NotNull
+	@Size(min = 10, max = 12)
+	@Digits(fraction = 0, integer = 12, message = "Incorrect Format, valid e.g. 121212121212")
+	@Column(name = "phone_number")
+	private String phoneNumber;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
